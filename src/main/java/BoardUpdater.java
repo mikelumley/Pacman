073daea6824cea2441b9b330f65c0e44da87b449
Michael Lumley <michael.lumley@myob.com>
2019-09-10 13:45:29 +1000

    Added wrapping to movement and added score when pacman eats food

--- src/main/java/BoardUpdater.java
+++ src/main/java/BoardUpdater.java
@@ -1,27 +1,66 @@
 public class BoardUpdater implements IBoardUpdater {
 
+    private int score = 0;
+
+    public int getScore() {
+        return this.score;
+    }
+
     @Override
     public Tile[][] movePacman(Tile[][] board, Direction direction) {
         Position pacmanPosition = this.findPacman(board);
         switch (direction) {
             case UP:
-                board[pacmanPosition.getX()][pacmanPosition.getY()].setObjectOnTile(GameObject.EMPTY);
-                board[pacmanPosition.getX() - 1][pacmanPosition.getY()].setObjectOnTile(GameObject.PACMAN);
+                this.moveUp(board, pacmanPosition);
                 break;
             case DOWN:
-                board[pacmanPosition.getX()][pacmanPosition.getY()].setObjectOnTile(GameObject.EMPTY);
-                board[pacmanPosition.getX() + 1][pacmanPosition.getY()].setObjectOnTile(GameObject.PACMAN);
+                this.moveDown(board, pacmanPosition);
                 break;
             case LEFT:
-                board[pacmanPosition.getX()][pacmanPosition.getY()].setObjectOnTile(GameObject.EMPTY);
-                board[pacmanPosition.getX()][pacmanPosition.getY() - 1].setObjectOnTile(GameObject.PACMAN);
+                this.moveLeft(board, pacmanPosition);
                 break;
             case RIGHT:
-                board[pacmanPosition.getX()][pacmanPosition.getY()].setObjectOnTile(GameObject.EMPTY);
-                board[pacmanPosition.getX()][pacmanPosition.getY() + 1].setObjectOnTile(GameObject.PACMAN);
+                this.moveRight(board, pacmanPosition);
                 break;
         }
         return board;
+    }
+
+    private void moveUp(Tile[][] board, Position position) {
+        int nextXPosition = (position.getX() + board.length - 1) % board.length;
+        Position nextPosition = new Position(nextXPosition, position.getY());
+        board[position.getX()][position.getY()].setObjectOnTile(GameObject.EMPTY);
+        this.calcScore(board, nextPosition);
+        board[nextPosition.getX()][position.getY()].setObjectOnTile(GameObject.PACMAN);
+    }
+
+    private void moveDown(Tile[][] board, Position position) {
+        int nextXPosition = (position.getX() + board.length + 1) % board.length;
+        Position nextPosition = new Position(nextXPosition, position.getY());
+        board[position.getX()][position.getY()].setObjectOnTile(GameObject.EMPTY);
+        this.calcScore(board, nextPosition);
+        board[nextXPosition][position.getY()].setObjectOnTile(GameObject.PACMAN);
+    }
+
+    private void moveLeft(Tile[][] board, Position position) {
+        int nextYPosition = (position.getY() + board.length - 1) % board.length;
+        Position nextPosition = new Position(position.getX(), nextYPosition);
+        board[position.getX()][position.getY()].setObjectOnTile(GameObject.EMPTY);
+        this.calcScore(board, nextPosition);
+        board[position.getX()][nextYPosition].setObjectOnTile(GameObject.PACMAN);
+    }
+
+    private void moveRight(Tile[][] board, Position position) {
+        int nextYPosition = (position.getY() + board.length + 1) % board.length;
+        Position nextPosition = new Position(position.getX(), nextYPosition);
+        board[position.getX()][position.getY()].setObjectOnTile(GameObject.EMPTY);
+        this.calcScore(board, nextPosition);
+        board[position.getX()][nextYPosition].setObjectOnTile(GameObject.PACMAN);
+    }
+
+    private void calcScore(Tile[][] board, Position nextPosition) {
+        if (board[nextPosition.getX()][nextPosition.getY()].getObjectOnTile() == GameObject.FOOD)
+            score++;
     }
 
     private Position findPacman(Tile[][] board) {
--- src/test/java/BoardUpdaterTests.java
+++ src/test/java/BoardUpdaterTests.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 public class BoardUpdaterTests {
 
     @Test
-    public void Given_33BoardWithPacmanAt11_When_PlayerSelectsUp_Then_PacmanMovesUp() {
+    public void Given_BoardWithPacmanAtMiddle_When_PlayerSelectsUp_Then_PacmanMovesUp() {
         char[] row1 = {' ', ' ', ' '};
         char[] row2 = {' ', 'P', ' '};
         char[] row3 = {' ', ' ', ' '};
@@ -23,7 +23,7 @@ public class BoardUpdaterTests {
     }
 
     @Test
-    public void Given_33BoardWithPacmanAt11_When_PlayerSelectsDown_Then_PacmanMovesDown() {
+    public void Given_BoardWithPacmanAtMiddle_When_PlayerSelectsDown_Then_PacmanMovesDown() {
         char[] row1 = {' ', ' ', ' '};
         char[] row2 = {' ', 'P', ' '};
         char[] row3 = {' ', ' ', ' '};
@@ -41,7 +41,7 @@ public class BoardUpdaterTests {
     }
 
     @Test
-    public void Given_33BoardWithPacmanAt11_When_PlayerSelectsLeft_Then_PacmanMovesLeft() {
+    public void Given_BoardWithPacmanAtMiddle_When_PlayerSelectsLeft_Then_PacmanMovesLeft() {
         char[] row1 = {' ', ' ', ' '};
         char[] row2 = {' ', 'P', ' '};
         char[] row3 = {' ', ' ', ' '};
@@ -59,7 +59,7 @@ public class BoardUpdaterTests {
     }
 
     @Test
-    public void Given_33BoardWithPacmanAt11_When_PlayerSelectsRight_Then_PacmanMovesRight() {
+    public void Given_BoardWithPacmanAtMiddle_When_PlayerSelectsRight_Then_PacmanMovesRight() {
         char[] row1 = {' ', ' ', ' '};
         char[] row2 = {' ', 'P', ' '};
         char[] row3 = {' ', ' ', ' '};
@@ -74,5 +74,129 @@ public class BoardUpdaterTests {
         char[][] expectedBoardAsChar = {expectedRow1, expectedRow2, expectedRow3};
         Tile[][] expected = BoardFactory.createBoardFromChars(expectedBoardAsChar);
         assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void Given_BoardWithPacmanAtMiddleTop_When_PlayerSelectsUp_Then_PacmanMovesToMiddleBottom() {
+        char[] row1 = {' ', 'P', ' '};
+        char[] row2 = {' ', ' ', ' '};
+        char[] row3 = {' ', ' ', ' '};
+        char[][] boardAsChar = {row1, row2, row3};
+        Tile[][] startingBoard = BoardFactory.createBoardFromChars(boardAsChar);
+        BoardUpdater updater = new BoardUpdater();
+        Tile[][] result = updater.movePacman(startingBoard, Direction.UP);
+
+        char[] expectedRow1 = {' ', ' ', ' '};
+        char[] expectedRow2 = {' ', ' ', ' '};
+        char[] expectedRow3 = {' ', 'P', ' '};
+        char[][] expectedBoardAsChar = {expectedRow1, expectedRow2, expectedRow3};
+        Tile[][] expected = BoardFactory.createBoardFromChars(expectedBoardAsChar);
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void Given_BoardWithPacmanAtMiddleBottom_When_PlayerSelectsDown_Then_PacmanMovesToMiddleTop() {
+        char[] row1 = {' ', ' ', ' '};
+        char[] row2 = {' ', ' ', ' '};
+        char[] row3 = {' ', 'P', ' '};
+        char[][] boardAsChar = {row1, row2, row3};
+        Tile[][] startingBoard = BoardFactory.createBoardFromChars(boardAsChar);
+        BoardUpdater updater = new BoardUpdater();
+        Tile[][] result = updater.movePacman(startingBoard, Direction.DOWN);
+
+        char[] expectedRow1 = {' ', 'P', ' '};
+        char[] expectedRow2 = {' ', ' ', ' '};
+        char[] expectedRow3 = {' ', ' ', ' '};
+        char[][] expectedBoardAsChar = {expectedRow1, expectedRow2, expectedRow3};
+        Tile[][] expected = BoardFactory.createBoardFromChars(expectedBoardAsChar);
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void Given_BoardWithPacmanAtMiddleLeft_When_PlayerSelectsLeft_Then_PacmanMovesToMiddleRight() {
+        char[] row1 = {' ', ' ', ' '};
+        char[] row2 = {'P', ' ', ' '};
+        char[] row3 = {' ', ' ', ' '};
+        char[][] boardAsChar = {row1, row2, row3};
+        Tile[][] startingBoard = BoardFactory.createBoardFromChars(boardAsChar);
+        BoardUpdater updater = new BoardUpdater();
+        Tile[][] result = updater.movePacman(startingBoard, Direction.LEFT);
+
+        char[] expectedRow1 = {' ', ' ', ' '};
+        char[] expectedRow2 = {' ', ' ', 'P'};
+        char[] expectedRow3 = {' ', ' ', ' '};
+        char[][] expectedBoardAsChar = {expectedRow1, expectedRow2, expectedRow3};
+        Tile[][] expected = BoardFactory.createBoardFromChars(expectedBoardAsChar);
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void Given_BoardWithPacmanAtMiddleRight_When_PlayerSelectsLeft_Then_PacmanMovesToMiddleLeft() {
+        char[] row1 = {' ', ' ', ' '};
+        char[] row2 = {' ', ' ', 'P'};
+        char[] row3 = {' ', ' ', ' '};
+        char[][] boardAsChar = {row1, row2, row3};
+        Tile[][] startingBoard = BoardFactory.createBoardFromChars(boardAsChar);
+        BoardUpdater updater = new BoardUpdater();
+        Tile[][] result = updater.movePacman(startingBoard, Direction.RIGHT);
+
+        char[] expectedRow1 = {' ', ' ', ' '};
+        char[] expectedRow2 = {'P', ' ', ' '};
+        char[] expectedRow3 = {' ', ' ', ' '};
+        char[][] expectedBoardAsChar = {expectedRow1, expectedRow2, expectedRow3};
+        Tile[][] expected = BoardFactory.createBoardFromChars(expectedBoardAsChar);
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void Given_BoardWithPacmanAtMiddle_When_PlayerSelectsUp_Then_PacmanMovesToMiddleTopAndEatsFood() {
+        char[] row1 = {'.', '.', '.'};
+        char[] row2 = {'.', 'P', '.'};
+        char[] row3 = {'.', '.', '.'};
+        char[][] boardAsChar = {row1, row2, row3};
+        Tile[][] startingBoard = BoardFactory.createBoardFromChars(boardAsChar);
+        BoardUpdater updater = new BoardUpdater();
+        updater.movePacman(startingBoard, Direction.UP);
+        int result = updater.getScore();
+        assertEquals(1, result);
+    }
+
+    @Test
+    public void Given_BoardWithPacmanAtMiddle_When_PlayerSelectsDown_Then_PacmanMovesToMiddleBottomAndEatsFood() {
+        char[] row1 = {'.', '.', '.'};
+        char[] row2 = {'.', 'P', '.'};
+        char[] row3 = {'.', '.', '.'};
+        char[][] boardAsChar = {row1, row2, row3};
+        Tile[][] startingBoard = BoardFactory.createBoardFromChars(boardAsChar);
+        BoardUpdater updater = new BoardUpdater();
+        updater.movePacman(startingBoard, Direction.DOWN);
+        int result = updater.getScore();
+        assertEquals(1, result);
+    }
+
+    @Test
+    public void Given_BoardWithPacmanAtMiddle_When_PlayerSelectsLeft_Then_PacmanMovesToMiddleLeftAndEatsFood() {
+        char[] row1 = {'.', '.', '.'};
+        char[] row2 = {'.', 'P', '.'};
+        char[] row3 = {'.', '.', '.'};
+        char[][] boardAsChar = {row1, row2, row3};
+        Tile[][] startingBoard = BoardFactory.createBoardFromChars(boardAsChar);
+        BoardUpdater updater = new BoardUpdater();
+        updater.movePacman(startingBoard, Direction.LEFT);
+        int result = updater.getScore();
+        assertEquals(1, result);
+    }
+
+    @Test
+    public void Given_BoardWithPacmanAtMiddle_When_PlayerSelectsRight_Then_PacmanMovesToMiddleRightAndEatsFood() {
+        char[] row1 = {'.', '.', '.'};
+        char[] row2 = {'.', 'P', '.'};
+        char[] row3 = {'.', '.', '.'};
+        char[][] boardAsChar = {row1, row2, row3};
+        Tile[][] startingBoard = BoardFactory.createBoardFromChars(boardAsChar);
+        BoardUpdater updater = new BoardUpdater();
+        updater.movePacman(startingBoard, Direction.RIGHT);
+        int result = updater.getScore();
+        assertEquals(1, result);
     }
 }
